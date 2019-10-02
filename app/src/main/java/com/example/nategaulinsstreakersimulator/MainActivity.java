61087458503152ae5cb85c795d1d351c7d5bb1a2
117EliaS <eliaspyratos1@gmail.com>
2019-10-02 13:49:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nategaulinsstreakersimulator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nategaulinsstreakersimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View v){
+
+        Intent intent = new Intent(this, GameScreenActivity.class);
+        startActivity(intent);
+    }
+}
