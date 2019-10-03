d16f755b9dd223f8a23afed1a122e58bf1a2fe9d
117EliaS <eliaspyratos1@gmail.com>
2019-10-03 13:46:59 -0500

    added timer for game

--- app/src/main/java/com/example/nategaulinsstreakersimulator/GameScreenActivity.java
+++ app/src/main/java/com/example/nategaulinsstreakersimulator/GameScreenActivity.java
@@ -3,12 +3,79 @@ package com.example.nategaulinsstreakersimulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class GameScreenActivity extends AppCompatActivity {
+
+    private int milsecs = 0;
+    private boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        runTimer();
+        startTimer();
+    }
+
+    // Start stopwatch when the game starts
+    public void startTimer(){
+
+        running = true;
+
+    }
+
+
+    // Stop stopwatch when the game ends
+    public void stopTimer(View v){
+
+        running = false;
+
+    }
+
+
+    // Reset stopwatch when the reset button is clicked
+    public void resetTimer(View v){
+
+        running = false;
+        milsecs = 0;
+
+    }
+
+    private void runTimer(){
+
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable(){
+            @Override
+            public void run() {
+
+
+                int milsec = milsecs % 10;
+
+                int secs = (milsecs / 10) % 60;
+
+                int minutes  = (milsecs / 600);
+
+
+                String time = String.format(Locale.getDefault(), "%02d:%02d:%01d", minutes, secs, milsec);
+
+                timeView.setText(time);
+
+                if(running){
+                    milsecs ++;
+                }
+                handler.postDelayed(this ,100);
+            }
+
+
+        });
+
+
+
     }
 }
