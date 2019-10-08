a52d525be0fade7b0f714845deee829bc5cd25b1
Patman526 <breenboyz@gmail.com>
2019-10-08 13:59:33 -0500

    Created Footballer class

---
+++ app/src/main/java/com/example/nategaulinsstreakersimulator/Footballer.java
@@ -0,0 +1,22 @@
+package com.example.nategaulinsstreakersimulator;
+
+public class Footballer {
+
+    private final int SPEED = 10;
+    private int posX;
+    private int posY;
+
+    public Footballer(int posX, int posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getSpeed() {
+        return SPEED;
+    }
+
+    public void move(int playerX, int playerY){
+        
+
+    }
+}
