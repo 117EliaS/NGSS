18fe034f2e896a2fb3c260968b4bbbcebb75a9cf
eyxs <xacriimony@gmail.com>
2019-11-01 13:47:52 -0500

    Nate Gaulin Streaking Simulator Game of the Year HD remastered 2.0 (REDUX) feat. Knuckles and Dante from DMC: 2: Super Turbo Edition 2: Electric Boogaloo 2 with new funky mode

--- app/src/main/java/com/example/nategaulinsstreakersimulator/endgame_activity.java
+++ app/src/main/java/com/example/nategaulinsstreakersimulator/endgame_activity.java
@@ -3,7 +3,10 @@ package com.example.nategaulinsstreakersimulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -25,6 +28,13 @@ public class endgame_activity extends AppCompatActivity {
         TextView score = (TextView) findViewById(R.id.score_view);
 
         score.setText("Time: " + scoreString);
+
+        ImageView gameoverscreen = (ImageView) findViewById(R.id.menuBackground);
+        BitmapDrawable gameoverscreen_drawable = (BitmapDrawable) getDrawable(R.drawable.gameoverscreen);
+        Bitmap gameoverscreen_bmp = Bitmap.createScaledBitmap(gameoverscreen_drawable.getBitmap(), 1920, 1920, false);
+        Drawable gameoverscreen_done = new BitmapDrawable(getResources(), gameoverscreen_bmp);
+        gameoverscreen.setBackground(gameoverscreen_done);
+
 
     }
 
